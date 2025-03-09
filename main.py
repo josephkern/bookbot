0e18261062100c91a07ca29a956a97180cabb1f7
josephkern <joseph.a.kern@gmail.com>
2025-03-09 14:39:26 +0300

    created stats module

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+from stats import word_count
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_content = f.read()
+    return file_content
+
+def main():
+    
+    raw_text = get_book_text("books/frankenstein.txt")
+    print(f'{word_count(raw_text)} words found in the document')
+    #print(f'{raw_text}')
+
+main()
