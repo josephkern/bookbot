e818aade81e73485311a94a77fe8dfb55e858761
josephkern <joseph.a.kern@gmail.com>
2025-03-09 16:09:42 +0300

    commented code clean up, course passed

--- main.py
+++ main.py
@@ -15,12 +15,9 @@ def main():
         sys.exit(1)
     else:
         book = sys.argv[1]
-
-    #book = "books/frankenstein.txt"
     
     raw_text = get_book_text(book)
-    #print(f'{raw_text}')
-    #Print Title
+
     print("============ BOOKBOT ============")
     print(f'Analyzing book found at {book}...')
     # print word count title
@@ -28,11 +25,11 @@ def main():
     print(f'Found {word_count(raw_text)} total words')
     # print character count tile
     print("--------- Character Count -------")
+    
     char_report_data = char_count_report(character_count(raw_text))
     for char_report in char_report_data:
-        #for i in char_report.items():
         print(f'{char_report["char"]}: {char_report["num"]}')
-    #print(character_count(raw_text))
+
     print("============= END ===============")
 
 main()
